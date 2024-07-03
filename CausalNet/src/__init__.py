99ee1bd32d735ece85de564d8ff7562e25ab0eb7
oussamanaji <m.oussama.naji@gmail.com>
2024-07-03 12:43:54 -0400

    Create __init__.py

---
+++ CausalNet/src/__init__.py
@@ -0,0 +1,10 @@
+from .hccl import HCCL
+from .ciat import CIAT
+from .mcr import MCR
+from .acrt import ACRT
+from .ecrt import ECRT
+from .mlca import MLCA
+from .ckis import CKIS
+from .clear_benchmark import CLEARBenchmark
+
+__all__ = ['HCCL', 'CIAT', 'MCR', 'ACRT', 'ECRT', 'MLCA', 'CKIS', 'CLEARBenchmark']
